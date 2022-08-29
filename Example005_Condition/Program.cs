77dfb719b63c7f5e23f80636d98745dbde93efa9
redkinkiril <redkinkiril@yandex.ru>
2022-08-29 18:35:34 +0300

    added Example 005 Codition

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("enter the user name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Yahoo, it's Masha!");
+}
+else
+{
+    Console.Write("Hi,");
+    Console.WriteLine(username);
+}
