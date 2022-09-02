0ac6f7dcacaebe2dd85e9013e7522d066b29c19f
redkinkiril <redkinkiril@yandex.ru>
2022-09-02 12:32:34 +0300

    added change code

--- Examples008_IntroMethod/Program.cs
+++ Examples008_IntroMethod/Program.cs
@@ -5,7 +5,7 @@
     if (arg3> result) result = arg3;
     return result;
 }
-int a1 = 15;
+int a1 = 112125;
 int b1 = 21;
 int c1 = 39;
 int a2 = 12;
@@ -15,8 +15,11 @@ int a3 = 13;
 int b3 = 23111;
 int c3 = 313;
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max(max1, max2, max3);
+//int max1 = Max(a1, b1, c1);
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
+//int max = Max(max1, max2, max3);
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+
 Console.WriteLine(max);
