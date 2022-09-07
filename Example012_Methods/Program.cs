05b2d964cc5f3bd448ff7d8a4cba5a1b9e418c38
redkinkiril <redkinkiril@yandex.ru>
2022-09-07 22:04:10 +0300

    added change code programe method

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -25,18 +25,43 @@
        //Console.WriteLine(year);
 
        //View 4
-       string Method4(int count, string text)
+       //string Method4(int count, string text)
+       //{
+       // int i = 0;
+       // string result = String.Empty;
+       // while (i < count)
+       // {
+       //     result = result +text;
+       //     i++;
+       // }
+       // return result;
+       //}
+
+       //string Method4(int count, string text)
+       //{
+        
+       //string result = String.Empty;
+       // for ( int i = 0; i < count; i++)
+        
+       // {
+       //     result = result +text;   
+       // }
+       // return result;
+       // }
+       //string res = Method4(10, "z");
+       //Console.WriteLine(res);
+
+       
+       //string res = Method4(10, "z");
+       //Console.WriteLine(res);
+
+       for (int i = 2; i <= 10; i++)
        {
-        int i = 0;
-        string result = String.Empty;
-        while (i < count)
-        {
-            result = result +text;
-            i++;
-        }
-        return result;
+         for (int j = 0; j <= 10; j++)
+          {
+            Console.WriteLine($"{i} x {j} = {i * j}");
+          }
+          Console.WriteLine();
        }
-       string res = Method4(10, "z");
-       Console.WriteLine(res);
     }
 }
