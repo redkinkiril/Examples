84374b0db750cf7a7793bd24ded712440b0410d0
redkinkiril <redkinkiril@yandex.ru>
2022-09-08 19:54:55 +0300

    added change code

--- Example013_worktext/Program.cs
+++ Example013_worktext/Program.cs
@@ -1,39 +1,31 @@
-﻿// Work in text.
+﻿// =====Work in text.
 // дан текст. в тексте нужно все пробелы заменить черточками,
 // маленькие буквы "к" заменить большими "К",
 // а большие "С" заменить маленькими "с".
 
-internal partial class Program
+string text = "- Я думаю, - сказал князь, улыбаясь, что,"
+            + "ежели бы вас послали вместо нашего милого Винценггероде,"
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+
+// string s = "qwerty"
+//             012
+// s[3] // r
+
+string Replace(string text, char oldValue, char newValue);
 {
-    private static int Main(string[] args)
+    string result = String.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
     {
-        string text = "- Я думаю, - сказал князь, улыбаясь, что,"
-    + "ежели бы вас послали вместо нашего милого Винценггероде,"
-    + "вы бы взяли приступом согласие прусского короля. "
-    + "Вы так красноречивы. Вы дадите мне чаю?";
-
-
-        // string s = "qwerty"
-        //             012
-        // s[3] // r
-
-        string Replace(string text,
-                       char oldValue,
-                       char newValue);
-        {
-            string result = string.Empty;
-
-            int length = text.Length;
-            for (int i = 0; i < length; i++)
-            {
-                if (text[i] == oldValue) result = result + $"{newValue}";
-                else result = result + $"{text[i]}";
-            }
-
-            return result;
-        }
-
-        string newText = Replace(text, ' ', '_');
-        Console.WriteLine(newText);
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
     }
+
+    return result;
 }
+
+string newText = Replace(text, ' ', '_');
+Console.WriteLine(newText);
