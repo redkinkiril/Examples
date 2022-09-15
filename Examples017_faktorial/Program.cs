586bf43bee6bb4590d1b6860308cd077afbfdae8
redkinkiril <redkinkiril@yandex.ru>
2022-09-15 20:27:57 +0300

    added Example017

---
+++ Examples017_faktorial/Program.cs
@@ -0,0 +1,48 @@
+﻿
+
+//int Factorial(int n)
+//{
+ // 1! = 1
+ // 0! = 1
+// if(n == 1) return 1;
+// else return n * Factorial(n-1);
+//}
+//Console.WriteLine(Factorial(3)); // 1 * 2 * 3 = 6
+
+//int Factorial(int n)
+//{
+ // 1! = 1
+ // 0! = 1
+// if(n == 1) return 1;
+// else return n * Factorial(n-1);
+//}
+//for (int i = 1; i < 40; i++)
+//{
+// Console.WriteLine($"{i}! = {Factorial(i)}");
+//}
+
+double Factorial(int n)
+{
+ // 1! = 1
+ // 0! = 1
+ if(n == 1) return 1;
+ else return n * Factorial(n-1);
+}
+for (int i = 1; i < 40; i++)
+{
+ Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+
+double Fibonacci(int n)
+{
+ if(n == 1 || n == 2) return 1;
+ else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+for (int i = 1; i < 10; i++)
+{
+ Console.WriteLine(Fibonacci(i));
+}
